5b81617d9ed1d03e568a83dab8f1f05ed2daded3
your_github_Hadilou1998 <your_github_hadilissilame@gmail.com>
2024-07-12 15:21:43 +0200

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,16 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Chat from "../components/Chat";
 import Nav from "../components/Nav";
 import Typed from 'typed.js';
+import dotenv from 'dotenv';
+
+// Interface pour le message de réponse
+interface Chat {
+  id: number;
+  question: string;
+  answer: string;
+  date: string;
+}
 
 export default function App() {
   const el = useRef(null);
@@ -19,12 +28,17 @@ export default function App() {
         typed.destroy();
       };
   }, []);
+  
+  const [IsResponded, setIsResponded] = useState<boolean>(false);
+  const [response, setResponse] = useState<string>('la réponse est vide');
+  const [question, setQuestion] = useState<string>('');
+  const [apiKey, setApiKey] = useState<string>(process.env.OPENAI_API_KEY || '');
 
   return (
-    <main className="bg-slate-50 p-5 flex flex-col items-center justify-between min-w-full min-h-screen">
+    <main className="flex flex-col items-center justify-between min-w-full min-h-screen p-5 bg-slate-50">
       <Nav />
       <section className="flex flex-col items-center justify-center">
-        <h1 className="text-6xl font-semibold mb-10">lya</h1>
+        <h1 className="mb-10 text-6xl font-semibold">lya</h1>
         <p className="text-xl"><span ref={el}></span></p>
       </section>
       <Chat />
